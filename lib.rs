2f6857502fcd098f90e0f05f5535f8735bc53192
Pham Hong Thai <hongthaipro@gmail.com>
2021-10-12 11:08:29 +0700

    Storing a Mapping

--- lib.rs
+++ lib.rs
@@ -7,17 +7,22 @@ mod incrementer {
     #[ink(storage)]
     pub struct Incrementer {
         value: i32,
+        my_value: ink_storage::collections::HashMap<AccountId,i32>,
     }
 
     impl Incrementer {
         #[ink(constructor)]
         pub fn new(init_value: i32) -> Self {
             // Contract Constructor
-            Self{value: init_value}
+            Self{
+                value: init_value,
+                my_value: Default::default()
+            }
         }
 
         pub fn default() -> Self {
-            Self{value: 0}
+            Self{value: 0, 
+                my_value: Default::default()}
         }
 
         #[ink(message)]
@@ -30,6 +35,20 @@ mod incrementer {
         pub fn inc(&mut self, by: i32) {
             // ACTION: Simply increment `value` by `by`
             self.value = self.value + by
+        }
+
+        #[ink(message)]
+        pub fn get_mine(&self) -> i32 {
+            // ACTION: Get `my_value` using `my_value_or_zero` on `&self.env().caller()`
+            // ACTION: Return `my_value`
+            let caller = &self.env().caller();
+            self.my_value_or_zero(&caller)
+        }
+
+        fn my_value_or_zero(&self, of: &AccountId) -> i32 {
+            // ACTION: `get` and return the value of `of` and `unwrap_or` return 0
+            let mv = self.my_value.get(of).unwrap_or(&0);
+            *mv
         }
     }
 
@@ -53,6 +72,14 @@ mod incrementer {
             assert_eq!(contract.get(), 47);
             contract.inc(-50);
             assert_eq!(contract.get(), -3);
+        }
+
+        // Use `ink::test` to initialize accounts.
+        #[ink::test]
+        fn my_value_works() {
+            let contract = Incrementer::new(11);
+            assert_eq!(contract.get(), 11);
+            assert_eq!(contract.get_mine(), 0);
         }
     }
 }
